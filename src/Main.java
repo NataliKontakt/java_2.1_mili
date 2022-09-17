ee290643e0cc7bb558f5a86344b0ee13ae484eff
“Natali <natali-kontakt@yandex.ru>
2022-09-17 09:28:51 +0500

    add java_2.1_mili

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 500; // Стоимость билета
+        int y = 20; // Стоимость одной мили
+        int z = 500/20; // Сколько миль начислено
+        System.out.println(z+" Миль начислено! Поздравляю!");
+    }
+}
