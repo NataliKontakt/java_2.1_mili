b54cd3a2d2a1489af97e6204db5a4b3746d035fc
“Natali <natali-kontakt@yandex.ru>
2022-09-17 11:23:52 +0500

    change src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int x = 500; // Стоимость билета
-        int y = 20; // Стоимость одной мили
-        int z = 500/20; // Сколько миль начислено
-        System.out.println(z+" Миль начислено! Поздравляю!");
+        int ticket_price = 500; // Стоимость билета
+        int mils_price = 20; // Стоимость одной мили
+        int summ = ticket_price/mils_price; // Сколько миль начислено
+        System.out.println(summ +" Миль начислено! Поздравляем!");
     }
 }
