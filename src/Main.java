e0be28b891663c17e0b188ce815ef8f41ef71984
“Natali <natali-kontakt@yandex.ru>
2022-09-18 21:10:40 +0500

    Reformat

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticket_price = 500; // Стоимость билета
-        int mils_price = 20; // Стоимость одной мили
-        int summ = ticket_price/mils_price; // Сколько миль начислено
-        System.out.println(summ +" Миль начислено! Поздравляем!");
+        int ticketPrice = 500; // Стоимость билета
+        int milsPrice = 20; // Стоимость одной мили
+        int summ = ticketPrice / milsPrice; // Сколько миль начислено
+        System.out.println(summ + " Миль начислено! Поздравляем!");
     }
 }
